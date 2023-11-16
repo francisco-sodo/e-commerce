005293cf715a11d65012ab11682b86e6c992bb17
Francisco Sodo <fanky1986@gmail.com>
2023-11-16 15:38:12 -0300

    ecommerce-react

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import NavBar from "./components/NavBar/NavBar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 
 
-
 function App(){
     return(
         <div className="App">
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,40 +4,39 @@ import brandLogo from "./assets/brand-logo.png";
 const NavBar = () => {
   return (
     <nav className="navbar is-link is-spaced">
-      <div className="container">
+      <div className="is-flex p-2 container">
 
-        <div className="navbar-brand">
+        <div className="container navbar-brand">
+          <figure>
             <img src={brandLogo} width={150} alt="logo" />
-         
+          </figure>
+
           <span class="navbar-burger" data-target="navbarMenuHeroA">
             <span></span>
             <span></span>
             <span></span>
           </span>
-
         </div>
 
         <div className="navbar-menu">
-          <div className="navbar-end">
-            <div className="buttons are-normal">
-              <button className="navbar-item button is-info is-light">
-                Categoria 1
-              </button>
-              <button className="navbar-item button is-info is-light">
-                Categoria 2
-              </button>
-              <button className="navbar-item button is-info is-light">
-                Categoria 3
-              </button>
-              <button className="navbar-item button is-info is-light">
-                Categoria 4
-              </button>
-            </div>
-          </div>
+          <ul className="navbar-menu">
+            <li className="navbar-item p-1">
+              <a href="#" className="button is-info is-light">Categoria 1</a>
+            </li>
+            <li className="navbar-item p-1">
+              <a href="#" className="button is-info is-light">Categoria 2</a>
+            </li>
+            <li className="navbar-item p-1">
+              <a href="#" className="button is-info is-light">Categoria 3</a>
+            </li>
+            <li className="navbar-item p-1">
+              <a href="#" className="button is-info is-light">Categoria 4</a>
+            </li>
+          </ul>
         </div>
 
-        <div className="navbar-end container">
-          <CartWidget number={0}/>
+        <div className="navbar-end">
+          <CartWidget number={0} />
         </div>
 
       </div>
